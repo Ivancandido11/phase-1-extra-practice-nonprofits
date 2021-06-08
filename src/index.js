5b792704fbef89d726d15dea78ac98236d05b9c3
Ivan Candido <ivan.candido@powerhrg.com>
2021-06-08 13:36:48 -0400

    Added functionality to click on nonprofit and show its detailed info

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 const nonProfitDiv = document.querySelector("#browse-nonprofits")
+const detailedInfoName = document.querySelector("#name")
+const detailedInfoImg = document.querySelector("#image")
+const detailedInfoDesc = document.querySelector("#description")
+const detailedInfoDonations = document.querySelector("#donations")
+
+const addDetailedInfo = (obj) => {
+  detailedInfoName.innerHTML = obj.name
+  detailedInfoImg.src = obj.image
+  detailedInfoDesc.innerHTML = obj.description
+  detailedInfoDonations.innerHTML = `$${obj.donations}`
+}
 
 const addNonprofitsToBar = () => {
   fetch("http://localhost:3000/nonprofits")
@@ -8,6 +19,9 @@ const addNonprofitsToBar = () => {
         const nonprofitsName = document.createElement("span")
         nonprofitsName.innerHTML = obj.name
         nonProfitDiv.append(nonprofitsName)
+        nonprofitsName.addEventListener("click", () => {
+          return addDetailedInfo(obj)
+        })
       })
     })
 }
