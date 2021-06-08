e7dde9085fb942329979dcb6343eb9a964ff40fa
Ivan Candido <ivan.candido@powerhrg.com>
2021-06-08 15:25:49 -0400

    Completed all Deliverables

--- src/index.js
+++ src/index.js
@@ -3,12 +3,43 @@ const detailedInfoName = document.querySelector("#name")
 const detailedInfoImg = document.querySelector("#image")
 const detailedInfoDesc = document.querySelector("#description")
 const detailedInfoDonations = document.querySelector("#donations")
+let detailedDonations
+const donation = document.querySelector("#donation-form")
 
 const addDetailedInfo = (obj) => {
   detailedInfoName.innerHTML = obj.name
   detailedInfoImg.src = obj.image
   detailedInfoDesc.innerHTML = obj.description
   detailedInfoDonations.innerHTML = `$${obj.donations}`
+  detailedInfoDonations.id = obj.id
+  detailedDonations = obj.donations
+}
+
+const donate = () => {
+  donation.addEventListener("submit", (event) => {
+    const id = detailedInfoDonations.id
+    event.preventDefault()
+    const donationValue = donation.querySelector("#donations").value
+    detailedDonations = parseInt(detailedDonations) + parseInt(donationValue)
+    console.log(detailedDonations)
+    const donationData = {
+      donations: parseInt(detailedDonations)
+    }
+    const configObject = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(donationData)
+    }
+    fetch(`http://localhost:3000/nonprofits/${id}`, configObject)
+      .then(resp => resp.json())
+      .then(nonprofit => {
+        console.log(nonprofit)
+        detailedInfoDonations.innerHTML = `$${nonprofit.donations}`
+      })
+  })
 }
 
 const addNonprofitsToBar = () => {
@@ -28,6 +59,7 @@ const addNonprofitsToBar = () => {
 
 const init = () => {
   addNonprofitsToBar()
+  donate()
 }
 
 document.addEventListener("DOMContentLoaded", init)
